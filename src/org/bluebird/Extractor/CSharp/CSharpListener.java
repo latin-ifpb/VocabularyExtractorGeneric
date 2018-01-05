4c75175a9f729fc51a68d51a554bd9cd0fc042b4
witoriamanuely <manuelycavalcante@gmail.com>
2018-01-05 17:26:59 -0300

    Adicionando extrator do namespace e do tipo da classe

--- src/org/bluebird/Extractor/CSharp/CSharpListener.java
+++ src/org/bluebird/Extractor/CSharp/CSharpListener.java
@@ -13,4 +13,16 @@ public class CSharpListener extends CSharpParserBaseListener {
         this.cSharpParser = parser;
         this.tokens = parser.getTokenStream();
     }
+    @Override
+    public void enterNamespace_declaration(CSharpParser.Namespace_declarationContext ctx) {
+        System.out.println(ctx.NAMESPACE() + " " + this.tokens.getText(ctx.qualified_identifier().getSourceInterval()) +" ");
+
+    }
+    public void enterType_declaration(CSharpParser.Type_declarationContext ctx) {
+        System.out.print("\t" + this.tokens.getText(ctx.all_member_modifiers().getSourceInterval()) + " ");
+    }
+    public void enterClass_base(CSharpParser.Class_baseContext ctx) {
+            System.out.print("\t" + ctx.class_type().STRING() + " ");
+    }
+
 }
