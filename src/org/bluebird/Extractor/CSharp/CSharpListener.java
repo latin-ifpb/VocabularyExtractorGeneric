00c7a489a5a863502e1fb4c38f7a437561a82cf4
witoriamanuely <manuelycavalcante@gmail.com>
2018-02-03 16:16:13 -0300

    Adicionando metodos que extraem o modificador, tipo, property (get e set), variavéis locais das: classes, metodos, e atributos.

--- src/org/bluebird/Extractor/CSharp/CSharpListener.java
+++ src/org/bluebird/Extractor/CSharp/CSharpListener.java
@@ -4,8 +4,6 @@ import org.antlr.v4.runtime.TokenStream;
 import org.bluebird.LanguagesUtil.CSharp.CSharpParser;
 import org.bluebird.LanguagesUtil.CSharp.CSharpParserBaseListener;
 
-import java.util.List;
-
 public class CSharpListener extends CSharpParserBaseListener {
 
     private CSharpParser cSharpParser;
@@ -46,18 +44,19 @@ public class CSharpListener extends CSharpParserBaseListener {
     public void enterMethod_declaration(CSharpParser.Method_declarationContext ctx) {
         String name = this.tokens.getText(ctx.method_member_name().identifier(0));
         String args;
+
         try {
             args = this.tokens.getText(ctx.formal_parameter_list());
         } catch (NullPointerException e) {  //Se der erro nao tem parametro
             args = "";
         }
-        System.out.println("\t\t" + name + "(" + args + ")");
+        System.out.println(name + "(" + args + ")");
     }
 
     @Override
     public void enterField_declaration(CSharpParser.Field_declarationContext ctx) {
         for ( CSharpParser.Variable_declaratorContext atributo : ctx.variable_declarators().variable_declarator()) {
-            System.out.println("\t\t" + this.tokens.getText(atributo.identifier().getSourceInterval()));
+            System.out.println(this.tokens.getText(atributo.identifier().getSourceInterval()));
         }
     }
 
@@ -70,7 +69,31 @@ public class CSharpListener extends CSharpParserBaseListener {
         } catch (NullPointerException e) {  //Se der erro nao tem parametro
             args = "";
         }
-        System.out.println("\t\t"+ name + "(" + args + ")");
+        System.out.println(name + "(" + args + ")");
+    }
+    @Override
+    public void enterAll_member_modifiers(CSharpParser.All_member_modifiersContext ctx) {
+        String soma = "";
+        for(int i = 0; i < ctx.all_member_modifier().size(); i++){
+            soma += this.tokens.getText(ctx.all_member_modifier(i)) + " ";
+        }
+        System.out.print("\t\t"+soma);
+    }
+    @Override
+    public void enterProperty_declaration(CSharpParser.Property_declarationContext ctx) {
+        System.out.println(this.tokens.getText(ctx.member_name().namespace_or_type_name().identifier(0)));
+    }
+
+    @Override
+    public void enterTyped_member_declaration(CSharpParser.Typed_member_declarationContext ctx) {
+        System.out.print(this.tokens.getText(ctx.type())+" ");
+
+    }
+    @Override
+    public void enterLocal_variable_declaration(CSharpParser.Local_variable_declarationContext ctx) {
+        String tipo = this.tokens.getText(ctx.local_variable_type().type());
+        String nome = this.tokens.getText(ctx.local_variable_declarator(0).identifier());
+        System.out.print("\t\t\t"+tipo+" "+nome+"\n");
     }
 
 }
