47cf3074597e5fb1a33dbf0b23db7ebc52a0cf8d
Reb0rn <anderson.nobrega62@gmail.com>
2018-02-04 14:22:04 -0300

    Organizando os prints do Listener de CSharp

--- src/org/bluebird/Extractor/CSharp/CSharpListener.java
+++ src/org/bluebird/Extractor/CSharp/CSharpListener.java
@@ -9,7 +9,7 @@ public class CSharpListener extends CSharpParserBaseListener {
     private CSharpParser cSharpParser;
     private TokenStream tokens;
 
-    public CSharpListener(CSharpParser parser) {
+    CSharpListener(CSharpParser parser) {
         this.cSharpParser = parser;
         this.tokens = parser.getTokenStream();
     }/*Construtor que passa o parser*/
@@ -17,28 +17,31 @@ public class CSharpListener extends CSharpParserBaseListener {
     @Override
     public void enterNamespace_declaration(CSharpParser.Namespace_declarationContext ctx) {
         System.out.println(ctx.NAMESPACE() + " " + this.tokens.getText(ctx.qualified_identifier().getSourceInterval()) +" ");
-
     }/* Extrai o namespace*/
 
     @Override
     public void enterType_declaration(CSharpParser.Type_declarationContext ctx) {
+        String modificadores = "";
         try {
-            System.out.print("\t" + this.tokens.getText(ctx.all_member_modifiers().getSourceInterval()) + " ");
-        } catch (NullPointerException e) { //Se der erro nao tem modificador na classe
-            System.out.print("\t");
+            for(int i = 0; i < ctx.all_member_modifiers().all_member_modifier().size(); i++){
+                modificadores += this.tokens.getText(ctx.all_member_modifiers().all_member_modifier(i)) + " ";
+            }
+        } catch (NullPointerException e) {
+            modificadores = "";
         }
 
+        System.out.print("\t" + modificadores);
     }/* Extrai o modificador da classe*/
 
     @Override
     public void enterClass_definition(CSharpParser.Class_definitionContext ctx) {
         try {
             String args = this.tokens.getText(ctx.type_parameter_list().type_parameter(0));
-            System.out.println(ctx.CLASS()+ " " + this.tokens.getText(ctx.identifier()) + " <" + args + ">");
+            System.out.println(ctx.CLASS() + " " + this.tokens.getText(ctx.identifier()) + " <" + args + ">");
         } catch (NullPointerException e) {  //Se der erro a classe nao tem parametro
-            System.out.println(ctx.CLASS()+ " " + this.tokens.getText(ctx.identifier()));
+            System.out.println(ctx.CLASS() + " " + this.tokens.getText(ctx.identifier()));
         }
-    }/* Extrai a clase */
+    }/* Extrai a classe */
 
     @Override
     public void enterMethod_declaration(CSharpParser.Method_declarationContext ctx) {
@@ -73,13 +76,18 @@ public class CSharpListener extends CSharpParserBaseListener {
     } /* Extrai o construtor*/
 
     @Override
-    public void enterAll_member_modifiers(CSharpParser.All_member_modifiersContext ctx) {
-        String soma = "";
-        for(int i = 0; i < ctx.all_member_modifier().size(); i++){
-            soma += this.tokens.getText(ctx.all_member_modifier(i)) + " ";
+    public void enterClass_member_declaration(CSharpParser.Class_member_declarationContext ctx) {
+        String modificadores = "";
+        try {
+            for(int i = 0; i < ctx.all_member_modifiers().all_member_modifier().size(); i++){
+                modificadores += this.tokens.getText(ctx.all_member_modifiers().all_member_modifier(i)) + " ";
+            }
+        } catch (NullPointerException e) {
+            modificadores = " ";
         }
-        System.out.print("\t\t"+soma);
-    }/* Extrai os modificadores das classes, metodos e atributos*/
+
+        System.out.print("\t\t" + modificadores);
+    }/* Extrai os modificadores de atributos e metodos da classe */
 
     @Override
     public void enterProperty_declaration(CSharpParser.Property_declarationContext ctx) {
@@ -88,15 +96,15 @@ public class CSharpListener extends CSharpParserBaseListener {
 
     @Override
     public void enterTyped_member_declaration(CSharpParser.Typed_member_declarationContext ctx) {
-        System.out.print(this.tokens.getText(ctx.type())+" ");
+        System.out.print(this.tokens.getText(ctx.type()) + " ");
 
     }/* Extrai os tipos das classes, dos metodos, e dos atributos*/
 
     @Override
     public void enterLocal_variable_declaration(CSharpParser.Local_variable_declarationContext ctx) {
-        String tipo = this.tokens.getText(ctx.local_variable_type().type());
+        String tipo = this.tokens.getText(ctx.local_variable_type());
         String nome = this.tokens.getText(ctx.local_variable_declarator(0).identifier());
-        System.out.print("\t\t\t"+tipo+" "+nome+"\n");
+        System.out.println("\t\t\t" + tipo + " " + nome);
     } /* Extrai a variavel local da classe*/
 
 }
