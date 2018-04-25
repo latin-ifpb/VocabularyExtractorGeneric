a8b233b70e71497f8ac2678da057c2ed45aab485
witoriamanuely <manuelycavalcante@gmail.com>
2018-04-25 10:40:47 -0300

    Adicionando metodos que extraem os tipos dos metodos, e os tipos dos atributos, bem como organizou-se outros metodos, e começou o incremento do vxl ao extrator Java. Adicionando comentários ao código.

--- src/org/bluebird/Extractor/Java/JavaListener.java
+++ src/org/bluebird/Extractor/Java/JavaListener.java
@@ -1,29 +1,88 @@
 package org.bluebird.Extractor.Java;
 
 import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.bluebird.LanguagesUtil.Java.JavaParser;
 import org.bluebird.LanguagesUtil.Java.JavaParserBaseListener;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.util.Stack;
 
 public class JavaListener extends JavaParserBaseListener {
 
     private TokenStream tokens;
+    private String modifiersClassInterface;
+    private String modifiersVariable;
+    private Stack<Integer> ruleIndex;
 
     JavaListener(JavaParser parser) {
         this.tokens = parser.getTokenStream();
+        this.ruleIndex = new Stack<>();
     }
+    /**
+      * Extrai o acesso das classes e interfarces
+     * @param ctx Entidade da Parser Tree
+     */
+    public void enterModifier(JavaParser.ModifierContext ctx) {
+        String temp = this.tokens.getText(ctx.classOrInterfaceModifier());
+        if (temp.equals("public") || temp.equals("private") || temp.equals("protected")){
+            this.modifiersClassInterface = temp;
+        }
 
+    }
+    /**
+     * Extrai as classes
+     * @param ctx Entidade da Parser Tree
+     */
     @Override
     public void enterClassDeclaration(JavaParser.ClassDeclarationContext ctx) {
-        System.out.println(ctx.CLASS() + " " + ctx.IDENTIFIER());
+        TerminalNode classIdentifier = ctx.IDENTIFIER();
+        //ruleIndex.push(ctx.getStart().getLine());
+        //FileCreator.appendToVxlFile("\t\t<class name=\"" + classIdentifier + "\" acess=\"" + this.modifiersClassInterface + "\">\n");
+
+        System.out.println(this.modifiersClassInterface + " " + ctx.CLASS() + " " + classIdentifier);
     }
+    /**
+     * Extrai os metodos
+     * @param ctx Entidade da Parser Tree
+     */
     @Override
     public void enterMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
-        System.out.println("\t" + ctx.IDENTIFIER());
+        TerminalNode methodIdentifier = ctx.IDENTIFIER();
+        String typeMethod = this.tokens.getText(ctx.typeTypeOrVoid());
+        System.out.println("\t" + typeMethod + " " + methodIdentifier);
     }
-
+    /**
+     * Extrai os atributos e seus respectivos tipos
+     * @param ctx Entidade da Parser Tree
+     */
     @Override
     public void enterFieldDeclaration(JavaParser.FieldDeclarationContext ctx) {
-        String atributo = tokens.getText(ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getSourceInterval());
-        System.out.println("\t" + atributo);
+        String fieldIdentifier =  this.tokens.getText(ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getSourceInterval());
+        String typeField = this.tokens.getText(ctx.typeType());
+        System.out.println("\t" + typeField + " " + fieldIdentifier);
     }
+    /**
+     * Extrai os construtores e seus respectivos parametros
+     * @param ctx Entidade da Parser Tree
+     */
+    @Override
+    public void enterConstructorDeclaration(JavaParser.ConstructorDeclarationContext ctx) {
+        String constructorParameters = this.tokens.getText(ctx.formalParameters());
+        TerminalNode constructorIdentifier = ctx.IDENTIFIER();
+        System.out.println("\t"+constructorIdentifier + " " +
+                constructorParameters);
+    }
+    /**
+     * Extrai o acesso das variaveis 
+     * @param ctx Entidade da Parser Tree
+     */
+    public void enterVariableModifier(JavaParser.VariableModifierContext ctx) {
+        String temp = this.tokens.getText(ctx.annotation());
+        System.out.println(temp);
+    }
+
+
+
+
 }
